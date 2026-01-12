d08a5ac0e7418094f0acbfe35500605f3e848c74
Atanas-Genchev <21527@uktc-bg.com>
2026-01-12 22:56:07 +0200

    made gun system

--- guns/gun.gd
+++ guns/gun.gd
@@ -1,19 +1,27 @@
 extends Node2D
+class_name Gun
 
 const bullet = preload("uid://cgiwjxpuksgxr")
-@onready var muzzel: Marker2D = $Marker2D
 @onready var firerate: Timer = $Firerate
+
+@export var automatic : bool = false
+var bullet_spawners : Array[Marker2D]
 var canfire : bool = true
 var orbit_distance: float 
+
 func _ready() -> void:
+	for child in get_children():
+		if child.name.to_lower().contains("bulletspawn"):
+			bullet_spawners.append(child)
 	orbit_distance = global_position.distance_to(get_parent().global_position)
+	
 func _process(_delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	var distance_to_mouse = global_position.distance_to(mouse_pos)
 	
 	var direction = (mouse_pos - get_parent().global_position).normalized()
 	global_position = get_parent().global_position + (direction * orbit_distance)
-	if distance_to_mouse > 5.0:
+	if distance_to_mouse > 2.0:
 		look_at(mouse_pos)
 	
 	rotation_degrees = wrapf(rotation_degrees, 0, 360)
@@ -21,18 +29,23 @@ func _process(_delta: float) -> void:
 		scale.y = -1
 	else:
 		scale.y = 1
-	
-	if Input.is_action_just_pressed("fire") and canfire:
-		fire()
+	if !canfire:
+		if Input.is_action_just_pressed("fire") and canfire:
+			fire()
+	else:
+		if Input.is_action_pressed("fire") and canfire:
+			fire()
 	
 func fire() -> void:
 		$AudioStreamPlayer2D.play()
 		firerate.start()
 		canfire = !canfire
-		var bullet_instance = bullet.instantiate()
-		get_tree().root.add_child(bullet_instance)
-		bullet_instance.global_position = muzzel.global_position
-		bullet_instance.rotation = rotation
+		for i in bullet_spawners:
+			print(i)
+			var bullet_instance = bullet.instantiate()
+			get_tree().root.add_child(bullet_instance)
+			bullet_instance.global_position = i.global_position
+			bullet_instance.rotation = i.global_rotation
 
 func _on_firerate_timeout() -> void:
 	canfire = !canfire
--- player/player.gd
+++ player/player.gd
@@ -16,4 +16,3 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	move_and_slide()
-	print(direction)
